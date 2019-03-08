cd63567165958f60808781fcd4e369cd494b1373
HYisen <alexhyisen@gmail.com>
2019-03-08 13:54:48 +0800

    introduce module-info

---
+++ world/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module pathfinder.world {
+    requires pathfinder.utility;
+    requires org.joml;
+    requires org.lwjgl;
+    requires org.lwjgl.natives;
+    requires org.lwjgl.opengl;
+    requires org.lwjgl.opengl.natives;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.glfw.natives;
+
+    opens shader;
+}
